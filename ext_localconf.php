1a206d54ec93bb628ab8f59f8afd4618c0b6e060
heilmann <heilmann@mindscreen.de>
2015-11-18 20:56:36 +0100

    Disable page access check for performance reason

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,7 +5,8 @@ if (!defined('TYPO3_MODE')) {
 }
 
 // Add deny check to page repository (actual page request)
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_page.php']['getPage']['denyfegroup'] = 'EXT:denyfegroup/Classes/PageRepositoryGetPage.php:B13\DenyFeGroup\PageRepositoryGetPage';
+// disabled for now for performance reasons
+//$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_page.php']['getPage']['denyfegroup'] = 'EXT:denyfegroup/Classes/PageRepositoryGetPage.php:B13\DenyFeGroup\PageRepositoryGetPage';
 
 // Add check to rootline (to respect "Extend to subpages" option)
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['hook_checkEnableFields']['denyfegroup'] = 'EXT:denyfegroup/Classes/GroupAccess.php:B13\DenyFeGroup\GroupAccess->checkEnableFields';
